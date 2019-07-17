50f882eb6b37f94e76da0ac6f0b30c9620fc38ea
unknown <s5902041620113@email.kmutnb.ac.th>
2019-07-17 22:11:18 +0700

    addsgnmetn3

---
+++ assignment3/practise.py
@@ -0,0 +1,9 @@
+my_list = [33,33,34]
+print(sum(my_list))
+
+my_tuble = (1,2,3,4,5)
+print(my_tuble)
+
+set1 = {1, 5, 12, 22, 77, 9}
+set2 = {5, 77, 9, 12, 13, 4, 25}
+print (set1.intersection(set2))
