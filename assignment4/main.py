6aadc0003e6d81b5dba4c8cda33660925fab4160
unknown <s5902041620113@email.kmutnb.ac.th>
2019-07-31 23:36:23 +0700

    assignment5

--- assignment4/main.py
+++ assignment4/main.py
@@ -1,14 +1,9 @@
 from flask import Flask
-from flask import render_template
-from flask import request
-
 app = Flask(__name__)
-@app.route("/")
-def index(): #โมดูลรับค่าจากไฟล์ index.html โดยการส่งค่าในรูปแบบ POST
-    return render_template('index.html')
-@app.route('/home', methods=['POST'])
-def save(): #โมดูลแสดงข้อมูล
-    return "Complete"
-app.run(debug=True)
+@app.route('/')
 
-    
+def index(): 
+   return 'Hello World' 
+
+if __name__ == '__main__':
+   app.run()
