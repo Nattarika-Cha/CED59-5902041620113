e2b7a510c19c59dcfabb4d103cba070d5b971c41
s5905041620113 <s5902041620113@email.kmutnb.ac.th>
2019-10-03 16:08:03 +0700

    assignment9

--- test8/common/config/main.php
+++ test8/common/config/main.php
@@ -8,8 +8,6 @@ return [
     'components' => [
         'cache' => [
             'class' => 'yii\caching\FileCache',
-            
-            
             ],
             'authManager' => [
                 'class' => 'yii\rbac\DbManager',
